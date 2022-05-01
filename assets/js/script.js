477059f2529d11465a04a080e1158c182d5eee47
dalla@MSI <dallas.archer.bailey@gmail.com>
2022-04-30 21:38:31 -0400

    added current date at top of page, save button and icon, initial variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,11 @@ var $scheduleArea = $(".container");
 var toDoEvents = [];
 
  
-var currentFullDate = moment().format("dddd, MMMM Do");
+var todaysDate = moment().format("dddd, MMMM Do, YYYY");
 var currentHour = moment().format("H");
 
-function 
+$currentDay.text(todaysDate);
+
+function initializeWorkDay(){
+
+}
